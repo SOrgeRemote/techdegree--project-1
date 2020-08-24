e99cafd4b8b4e66c9fdd6e17a60c1bee2115ca25
SOrgeRemote <66142523+SOrgeRemote@users.noreply.github.com>
2020-08-24 19:25:25 -0400

    added comments

--- js/script.js
+++ js/script.js
@@ -22,7 +22,6 @@ let quotes = [
   number2 = {
     quote: 'The most wasted of all days is one without laughter',
     source: 'Nicholas Chamfort',
-    citation: 'goodreads.com',
     year: 1769
   },
 
@@ -32,7 +31,7 @@ let quotes = [
     citation: 'Peter Pan',
     year: 1904
   },
-
+  //need at least one quote with missing properties for if statements
   number4 = {
     quote: 'To be creative means to be in love with life. You can be creative only if you love life enough that you want to enhance its beauty, you want to bring a little more music to it, a little more poetry to it, a little more dance to it.',
     source: 'Osho',
@@ -42,16 +41,16 @@ let quotes = [
     quote: 'If I were not a physicist, I would probably be a musician. I often think in music. I live my daydreams in music. I see my life in terms of music.',
     source: 'Albert Einstein',
     citation: 'goodreads.com',
-    year: 1929
   }
 ];
 
 // console.log(quotes);
+
 /***
  * `getRandomQuote` function
 ***/
 function getRandomQuote() {
-  const randomNumber = Math.floor(Math.random() * quotes.length);
+  const randomNumber = Math.floor(Math.random() * quotes.length); // quotes.length caps max random number
   // console.log(randomNumber);
   return quotes[randomNumber];
 }
@@ -64,30 +63,25 @@ function printQuote() {
   const randomQuote = getRandomQuote();
   console.log(randomQuote);
   let message = `
-    <p class="quote"> ${randomQuote.quote} </p>
+    <p class="quote"> ${randomQuote.quote} </p> // used template literal to inject javascript
     <p class="source"> ${randomQuote.source}`;
-    
-    if (randomQuote.citation) {
-      message += `<span class="citation"> ${randomQuote.citation} </span>`
-    }
 
-    if (randomQuote.year) {
-      message += `<span class="year"> ${randomQuote.year} </span>`
-    }
-    message += `</p>`;
-  document.getElementById('quote-box').innerHTML = message; 
+  // check if citation is truthy
+  if (randomQuote.citation) {
+    message += `<span class="citation"> ${randomQuote.citation} </span>` // used  += to add on to message
+  }
+
+  // check if year is truthy
+  if (randomQuote.year) {
+    message += `<span class="year"> ${randomQuote.year} </span>`
+  }
+
+  // finally always add </p> tag
+  message += `</p>`;
+
+  // update quote in window
+  document.getElementById('quote-box').innerHTML = message;
 }
-
-
-
-
-
-
-
-
-
-
-
 
 /***
  * click event listener for the print quote button
